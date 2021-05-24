56835e133433fafefc843975fae90b1e849e8d7b
Milan Tair <milan.tair@gmail.com>
2021-05-24 17:27:54 +0200

    Initalize a new React project

---
+++ 04-front-end/src/components/Application/Application.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Application.css';
+
+export default function Application() {
+  return (
+    <div className="Application">
+      ...
+    </div>
+  );
+}
---
+++ 04-front-end/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Application from './components/Application/Application';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
