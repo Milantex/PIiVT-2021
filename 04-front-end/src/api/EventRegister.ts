0efd58b7c5f3deceab956160ab93b6b25438098a
Milan Tair <milan.tair@gmail.com>
2021-05-28 15:31:33 +0200

    Create a master event registry for the application

---
+++ 04-front-end/src/api/EventRegister.ts
@@ -0,0 +1,19 @@
+import EventEmitter from "eventemitter3";
+
+const eventRegister = new EventEmitter();
+
+type EventType = "AUTH_EVENT" |
+                 "CART_EVENT" ;
+
+type EventHandler = (...args: any[]) => void;
+
+const EventRegister = {
+    on:   (event: EventType, handler: EventHandler) => eventRegister.on(event, handler),
+    once: (event: EventType, handler: EventHandler) => eventRegister.once(event, handler),
+    off:  (event: EventType, handler: EventHandler) => eventRegister.off(event, handler),
+    emit: (event: EventType, data: any)             => eventRegister.emit(event, data),
+};
+
+Object.freeze(EventRegister);
+
+export default EventRegister;
