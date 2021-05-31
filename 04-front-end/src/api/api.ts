36eeed34a50be7f92041c6a3eb29e2b4476fb7d4
Milan Tair <milan.tair@gmail.com>
2021-05-31 16:22:03 +0200

    Create a centralized role logged in check method

--- 04-front-end/src/api/api.ts
+++ 04-front-end/src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { AppConfiguration } from '../config/app.config';
 
 type ApiMethod = 'get' | 'post' | 'put' | 'delete';
-type ApiRole   = 'user' | 'administrator';
+export type ApiRole   = 'user' | 'administrator';
 type ApiResponseStatus = 'ok' | 'error' | 'login';
 
 export interface ApiResponse {
@@ -138,4 +138,17 @@ function refreshTokenResponseHandler(res: AxiosResponse<any>, resolve: (data: st
     }
 
     resolve(res.data?.authToken);
+}
+
+export function isRoleLoggedIn(role: ApiRole): Promise<boolean> {
+    return new Promise<boolean>(resolve => {
+        api("get", "/auth/" + role + "/ok", role)
+        .then(res => {
+            if (res?.data === "OK") return resolve(true);
+            resolve(false);
+        })
+        .catch(() => {
+            resolve(false);
+        })
+    });
 }
